873fb955fd593e1f868c1ad89a7ce9fdd9943123
Johnny Oskarsson <johnny@joskar.se>
2017-09-24 16:09:47 +0200

    Remember last category when rebuilding

--- categoryselector.py
+++ categoryselector.py
@@ -11,17 +11,23 @@ class CategorySelector(QWidget):
         self.grid.addWidget(self.combo)
         self.setLayout(self.grid)
         self.db = None
+        self.lastcategory = None
+        def updatelastcategory(index):
+            self.lastcategory = self.text(index)
+        self.combo.currentIndexChanged.connect(updatelastcategory)
 
     def connectdb(self, db):
         self.db = db
         self.buildlist()
 
     def buildlist(self):
+        lastcategory = self.lastcategory
         if self.db is None or len(self.db.root.documents) == 0:
             return
         self.combo.clear()
         graph = self.db.root.draw().split('\n')
         self.combo.addItems([x for x in graph if x.split()[-1].isidentifier()])
+        self.selectcategory(lastcategory)
 
     def callback(self, func):
         def clb(index):
@@ -35,3 +41,12 @@ class CategorySelector(QWidget):
             return self.combo.itemText(index).split()[-1]
         else:
             return self.combo.itemText(self.combo.currentIndex()).split()[-1]
+
+    def selectcategory(self, category=None):
+        if category is None:
+            self.combo.setCurrentIndex(0)
+        else:
+            for i in range(len(self.combo)):
+                if self.text(i) == category:
+                    self.combo.setCurrentIndex(i)
+                    return
