6b5f4f17cfe70b5b0d9db5bf1bf01a12eb172056
Johnny Oskarsson <johnny@joskar.se>
2017-09-24 09:37:16 +0200

    Move things to DocumentTreeView

---
+++ documentview.py
@@ -0,0 +1,100 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from icon import Icon
+
+
+class DocumentTreeView(QWidget):
+    def __init__(self, parent=None):
+        super(DocumentTreeView, self).__init__(parent)
+
+        self.tree = QTreeView()
+        self.tree.header().hide()
+        self.tree.setIndentation(20)
+        self.model = QStandardItemModel()
+
+        self.db = None
+        self.editview = None
+        self.icons = Icon()
+
+        oldSelectionChanged = self.tree.selectionChanged
+        def selectionChanged(selected, deselected):
+            if self.editview is not None:
+                si = selected.indexes()
+                di = deselected.indexes()
+                if selected and deselected:
+                    if len(di) > 0:
+                        data = self.model.data(di[0], Qt.UserRole + 2)
+                        data.text = self.editview.text()
+                if len(si) > 0:
+                    data = self.model.data(si[0], Qt.UserRole + 2)
+                    self.editview.settext(data.text)
+                else:
+                    self.editview.settext('')
+
+            oldSelectionChanged(selected, deselected)
+        self.tree.selectionChanged = selectionChanged
+        self.tree.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.tree.customContextMenuRequested.connect(self.contextmenu)
+        self.tree.setModel(self.model)
+
+        self.grid = QVBoxLayout()
+        self.grid.addWidget(self.tree)
+        self.setLayout(self.grid)
+
+    def contextmenu(self, pos):
+        menu = QMenu(parent=self.tree)
+        si = self.tree.selectedIndexes()
+        if len(si) > 0:
+            act = menu.addAction(self.icons.DialogSaveButton, 'Commit changes')
+            act.setEnabled(False)
+            act = menu.addAction(self.icons.DialogDiscardButton, 'Discard changes')
+            act.setEnabled(False)
+            menu.addSeparator()
+
+            data = self.model.data(si[0], Qt.UserRole + 2)
+            menu.addAction(self.icons.FileIcon, 'Create sibling document')
+            act = menu.addAction(self.icons.FileIcon, 'Create child document')
+            if str(data.level).split('.')[-1] != '0':
+                act.setEnabled(False)
+
+            menu.addSeparator()
+            act = menu.addAction('Remove document')
+            act.setEnabled(False)
+        else:
+            menu.addAction(self.icons.FileIcon, 'Create document')
+        menu.addSeparator()
+        menu.addAction('Expand all').triggered.connect(lambda: self.tree.expandAll())
+        def collapse():
+            self.tree.collapseAll()
+            self.tree.clearSelection()
+        menu.addAction('Collapse all').triggered.connect(collapse)
+        menu.popup(self.tree.mapToGlobal(pos))
+
+    def buildtree(self, cat=None):
+        if self.db is None or len(self.db.documents) == 0:
+            return
+        if cat is None:
+            cat = self.db.documents[0].prefix
+        c = [x for x in self.db if x.prefix == cat][0]
+        items = {}
+        for doc in sorted(c, key=lambda x: x.level):
+            level = str(doc.level)
+            title = '{} {}{:03}'.format(level, doc.prefix, doc.number)
+            item = QStandardItem(title)
+            item.setData(doc, Qt.UserRole + 2)
+            items[level] = item
+            up = level.split('.')
+            up[-1] = '0'
+            up = '.'.join(up)
+            if up != level and up in items:
+                items[up].appendRow(item)
+            else:
+                self.model.appendRow(item)
+
+    def connectdb(self, db):
+        self.db = db
+        self.buildtree()
+
+    def connectview(self, view):
+        self.editview = view
--- mainview.py
+++ mainview.py
@@ -5,104 +5,26 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from markdownview import MarkdownView
 import doorstop
+from documentview import DocumentTreeView
 
-
-def buildtree(cat, root):
-    t = doorstop.core.builder.build()
-    c = [x for x in t if x.prefix == cat][0]
-    items = {}
-    for doc in sorted(c, key=lambda x: x.level):
-        level = str(doc.level)
-        title = '{} {}{:03}'.format(level, doc.prefix, doc.number)
-        item = QStandardItem(title)
-        item.setData(doc, Qt.UserRole + 2)
-        items[level] = item
-        up = level.split('.')
-        up[-1] = '0'
-        up = '.'.join(up)
-        if up != level and up in items:
-            items[up].appendRow(item)
-        else:
-            root.appendRow(item)
-
-
-class Icon(object):
-    def __init__(self):
-        style = QCommonStyle()
-        icons = [x for x in dir(QStyle) if x.startswith('SP_')]
-        self.names = []
-        for name in icons:
-            icon = style.standardIcon(getattr(QStyle, name))
-            setattr(self, name[3:], icon)
-            self.names.append(name[3:])
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
 
-    icons = Icon()
     splitter = QSplitter()
     splitter.resize(1024, 768)
 
     v = MarkdownView()
 
-    tree = QTreeView()
-    model = QStandardItemModel()
+    db = doorstop.core.builder.build()
+    tree = DocumentTreeView()
+    tree.connectdb(db)
+    tree.connectview(v)
     splitter.addWidget(tree)
     splitter.addWidget(v)
     splitter.setSizes([320, 768])
-    tree.setContextMenuPolicy(Qt.CustomContextMenu)
-    def treecontextmenu(pos):
-        menu = QMenu(parent=tree)
-        si = tree.selectedIndexes()
-        if len(si) > 0:
-            act = menu.addAction(icons.DialogSaveButton, 'Commit changes')
-            act.setEnabled(False)
-            act = menu.addAction(icons.DialogDiscardButton, 'Discard changes')
-            act.setEnabled(False)
-            menu.addSeparator()
-
-            data = model.data(si[0], Qt.UserRole + 2)
-            menu.addAction(icons.FileIcon, 'Create sibling document')
-            act = menu.addAction(icons.FileIcon, 'Create child document')
-            if str(data.level).split('.')[-1] != '0':
-                act.setEnabled(False)
-
-            menu.addSeparator()
-            act = menu.addAction('Remove document')
-            act.setEnabled(False)
-        else:
-            menu.addAction(icons.FileIcon, 'Create document')
-        menu.addSeparator()
-        menu.addAction('Expand all').triggered.connect(lambda: tree.expandAll())
-        def collapse():
-            tree.collapseAll()
-            tree.clearSelection()
-        menu.addAction('Collapse all').triggered.connect(collapse)
-        menu.popup(tree.mapToGlobal(pos))
-    tree.customContextMenuRequested.connect(treecontextmenu)
-
-    tree.header().hide()
-    tree.setIndentation(20)
-
-    oldSelectionChanged = tree.selectionChanged
-    def selectionChanged(selected, deselected):
-        si = selected.indexes()
-        di = deselected.indexes()
-        if selected and deselected:
-            if len(di) > 0:
-                data = model.data(di[0], Qt.UserRole + 2)
-                data.text = v.text()
-        if len(si) > 0:
-            data = model.data(si[0], Qt.UserRole + 2)
-            v.settext(data.text)
-        else:
-            v.settext('')
-        oldSelectionChanged(selected, deselected)
-    tree.selectionChanged = selectionChanged
-    tree.setModel(model)
 
     splitter.show()
-    buildtree('req', model)
 
     sys.exit(app.exec_())
