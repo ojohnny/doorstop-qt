9f719e748b1c7305f9fb8ff96409daf1c57bfeba
Johnny Oskarsson <johnny@joskar.se>
2017-09-24 23:24:46 +0200

    Add ability to create parent links

---
+++ linkview.py
@@ -0,0 +1,61 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from markdown import markdown
+
+
+class LinkView(QListView):
+    def __init__(self, parent=None):
+        super(LinkView, self).__init__(parent)
+
+        self.db = None
+        self.model = QStandardItemModel()
+        self.setModel(self.model)
+        self.setAlternatingRowColors(True)
+        self.linkentry = None
+        def dataChanged(index):
+            if self.db is None:
+                return
+            if self.currentuid is None:
+                return
+            item = self.model.itemFromIndex(index)
+            data = item.data()
+            uid = item.text()
+            if data is None:
+                doc = self.db.find(uid)
+                if doc is not None:
+                    self.db.root.link_items(self.currentuid, uid)
+            else:
+                doc = self.db.find(uid)
+                if uid == '' or doc is not None:
+                    olduid = str(data.uid)
+                    self.db.root.unlink_items(self.currentuid, olduid)
+                if doc is not None:
+                    self.db.root.link_items(self.currentuid, uid)
+
+            self.read(self.currentuid)
+        self.model.dataChanged.connect(dataChanged)
+
+        self.clicked.connect(lambda s: self.edit(s))
+
+    def connectdb(self, db):
+        self.db = db
+
+    def read(self, uid):
+        if self.db is None:
+            return
+
+        self.currentuid = None
+
+        data = self.db.find(uid)
+        self.model.clear()
+        self.linkentry = QStandardItem('<Click here to add parent link>')
+        self.model.appendRow(self.linkentry)
+        for link in data.links:
+            d = self.db.find(str(link))
+            text = QTextDocument()
+            text.setHtml(markdown(d.text.split('\n')[0]))
+            item = QStandardItem(str(link) + '\t' + text.toPlainText())
+            item.setData(d)
+            self.model.appendRow(item)
+
+        self.currentuid = uid
--- mainview.py
+++ mainview.py
@@ -8,6 +8,7 @@ import doorstop
 from documentview import DocumentTreeView
 from createcatdiag import CreateCategoryDialog
 from attributeview import AttributeView
+from linkview import LinkView
 
 
 class ReqDatabase(object):
@@ -49,17 +50,19 @@ if __name__ == '__main__':
     createcatdiag = CreateCategoryDialog()
 
     attribview = AttributeView()
+    linkview = LinkView()
 
     tree = DocumentTreeView()
     tree.connectview(v)
     tree.connectcreatecatdiag(createcatdiag)
     def selectfunc(uid):
         attribview.read(uid)
+        linkview.read(uid)
         v.read(uid)
     tree.selectionclb = selectfunc
 
     db = ReqDatabase()
-    db.add_listeners(attribview)
+    db.add_listeners([attribview, linkview])
     v.readfunc = lambda uid: db.find(uid).text
     def savefunc(uid, text):
         db.find(uid).text = text
@@ -69,13 +72,23 @@ if __name__ == '__main__':
 
     editor = QWidget()
     editorgrid = QVBoxLayout()
+    editorgrid.setContentsMargins(0, 0, 0, 0)
     editorgrid.addWidget(attribview)
     editorgrid.addWidget(v)
     editor.setLayout(editorgrid)
 
+    rview = QWidget()
+    rviewgrid = QVBoxLayout()
+    rview.setLayout(rviewgrid)
+    vsplitter = QSplitter(Qt.Vertical)
+    vsplitter.addWidget(editor)
+    vsplitter.addWidget(linkview)
+    vsplitter.setStretchFactor(0, 100)
+    rviewgrid.addWidget(vsplitter)
+
     splitter.addWidget(tree)
-    splitter.addWidget(editor)
-    splitter.setSizes([320, 768])
+    splitter.addWidget(rview)
+    splitter.setStretchFactor(100, 1)
 
     splitter.show()
 
